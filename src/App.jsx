da9fd1cf40234a7b6317d03ca4452d67bfc87de8
haiaza <ajames1342@gmail.com>
2024-10-10 10:55:36 -0400

    Signup pre-stretch goal complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Signup from './pages/Signup'                                                                                                                                                                                                                                                           
+import { useState } from 'react'
+
+
 
 function App() {
+  const [user, setUser] = useState(null)
+  
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup setUser = {setUser} />} />
     </Routes>
   )
 }
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+    const Signup = (props) => {
+    const navigate = useNavigate()
+    const [message, setMessage] = useState([""])
+    const [userData, setUserData] = useState({
+            username: '',
+            password: '',
+            passwordConf: ''
+    })
+
+    const handleChange = (e) => {
+        setUserData({ ...userData, [e.target.name]: e.target.value });
+    }
+
+
+    const updateMessage = (msg) => {
+        setMessage(msg)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+          props.setUser(userData) // this will modify the state in the App component
+          navigate('/') // upon redirect you will see the "Dashboard" page
+        } catch (err) {
+            updateMessage({ruhRoh: err.message})
+        }
+    }
+
+    const { username, password, passwordConf } = userData
+
+    const isFormInvalid = () => {
+        return !(username && password && password === passwordConf);
+    }
+
+    const isPWThere = () => {
+        return !(password.length)
+    }
+
+    return (
+        <main>
+            <h1>Sign Up</h1>
+            <p>{message}</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                type="text"
+                    id="name"
+                    value={username}
+                    name="username"
+                    onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    name="password"
+                    onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirm">Confirm Password:</label>
+                    <input
+                        type="password"
+                        id="confirm"
+                        value={passwordConf}
+                        name="passwordConf"
+                        onChange={handleChange}
+                        disabled={isPWThere()}
+                    />
+                </div>
+                <div>
+                    <button disabled={isFormInvalid()}>Sign Up</button>
+                    <Link to="/">
+                        <button>Cancel</button>
+                    </Link>
+                </div>
+            </form>
+        </main>
+    )
+}
+
+export default Signup
