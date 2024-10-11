e16e4c6367621efdd4af023147f158a7b6506943
haiaza <ajames1342@gmail.com>
2024-10-11 17:32:57 -0400

    preq.params isnt working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+// import req from 'express/lib/request';
+import req from 'express/lib/request';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ user }) => {
@@ -10,11 +12,20 @@ const Navbar = ({ user }) => {
                     <li><Link to="">Sign Out</Link></li>
                 </ul>
             </nav>
-        ) : (
+        ) :
+        user && req.params.signin === 'signin' ? (
+            <nav>
+                <ul>
+                    <li>NOTHING</li>
+                    <li><Link to="/testing">Dashboard</Link></li>
+                </ul>
+            </nav>
+        ) :
+        (
             <nav>
                 <ul>
                     <li><Link to="/signin">Sign In</Link></li>
-                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><Link to="/testing">Dashboard</Link></li>
                 </ul>
             </nav>
         )}
