3fffedd2b691ff803af29be6f500421a9d053b42
haiaza <ajames1342@gmail.com>
2024-10-11 18:11:54 -0400

    pdated to useLocation. add things to check later

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,28 @@
-// import req from 'express/lib/request';
-import req from 'express/lib/request';
+import { useLocation, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ user }) => {
+
+    const { param } = useParams()
+    const location = useLocation()
+
+    
+    if (location.pathname === '/signup') {
+        return null
+    }
     return (
         <>
         { user ? (
+                /**Check if the My Pets looks/acts as expected with internet */
             <nav>
                 <ul>
                     <li><Link to="/profiles/">Home</Link></li>
                     <li><Link to="">Sign Out</Link></li>
+                    <li><Link to="/profiles/:userId/pets">My Pets</Link></li> 
                 </ul>
             </nav>
         ) :
-        user && req.params.signin === 'signin' ? (
+        param === 'signin' ? ( /**CHECK IF THIS WORKS AS EXPECTED WITH INTERNET */
             <nav>
                 <ul>
                     <li>NOTHING</li>
