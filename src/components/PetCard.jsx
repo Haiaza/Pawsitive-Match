597e7cc2f5c40a27d77c0f78cb959e119c5c769c
haiaza <ajames1342@gmail.com>
2024-10-17 22:51:59 -0400

    updated so stats show + accurate unique link

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -4,11 +4,11 @@ import '../styles/index.scss'
 const PetCard = ({ pet }) => {
     return  (
         <div className="pet-card">
-            <Link to={`/pet/${pet.id}`}>
+            <Link to={`/pets/${pet._id}`}>
                 <img src={pet.pic} alt={pet.breed} />
                 <div className="pet-info">
                     <h3>{pet.name}</h3>
-                    <p>{pet.age}</p>
+                    <p>{pet.Age}</p>
                 </div>
             </Link>
         </div>
