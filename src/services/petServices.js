1426622f84372e8ee8718787d6b02ce94a1eaa52
haiaza <ajames1342@gmail.com>
2024-10-18 01:13:41 -0400

    testing

--- src/services/petServices.js
+++ src/services/petServices.js
@@ -23,11 +23,15 @@ const populateUserPets = async ({ user }) => {
     console.log('populateUserPets is working')
 
     const userId =  user._id
-
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlN0cmluZyIsIl9pZCI6IjY3MDQ4Y2M4NGY2N2ZmYmUyYzk1MTQxYSIsImlhdCI6MTcyOTIyNzA0M30.FWAUyJfsmcvljZjgrfk3E_cIY8MLm2BRfFuTxmEBZY4"  //remove after testing 
     try {
         const res = await fetch(`${BACKEND_URL}/profiles/${userId}`, {
             method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 
+                'Content-Type': 'application/json', 
+                'Authorization': `Bearer ${token}`,
+            },
+
         })
         const json = await res.json();
         if (json.error) {
