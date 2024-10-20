616c7eda00a20f6903b73f976189df27eda6d13b
haiaza <ajames1342@gmail.com>
2024-10-20 13:21:25 -0400

    i have a plan to pass down the petDb state

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import Dashboard from './pages/Dashboard'
 import Signup from './pages/Signup'     
 import Signin from './pages/Signin'
 import UserPets from './pages/UserPets'        
-import ShowPet from './pages/ShowPet'                                                                                                                                                                                                                                         
+import ShowPet from './pages/ShowPet'
+import PetSubmissionForm from './pages/PetSubMissionForm'                                                                                                                                                                                                                                         
 import Navbar from './components/Navbar'
 import { useState } from 'react'
 
 import * as authService from './services/authService'
+import petServices from './services/petServices'
 
 
 function App() {
   const [myUser, setMyUser] = useState(authService.getUser())
+  const [petDb, setPetDb] = useState(petServices.populatePets())
+
+  console.log(petDb.then((res) => {
+    console.log(res)
+  }))
 
   console.log(myUser)
   
@@ -27,6 +34,7 @@ function App() {
         <Route path="/signin" element={<Signin setUser = {setMyUser} />} />
         <Route path="/pets/:id" element={<ShowPet />} />
         <Route path="/profiles/:userId" element={<UserPets user={myUser}/>} />
+        <Route path='/TEST' element={<PetSubmissionForm />} />
       </Routes>
       </>
   )
