8396e53a38d682ba3d77a5a17be31c6493336b20
haiaza <ajames1342@gmail.com>
2024-10-20 02:30:32 -0400

    RandomDog image added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom"
 import '../styles/index.scss'
+import { useEffect, useState } from "react"
 
 const Landing = () => {
+
+    const [dogSrc, setDogSrc] = useState("")
+
+    const randomDogImg = async () => {
+        const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+        const data = await res.json()
+        
+        console.log(data.message)
+
+        setDogSrc(data.message)
+    }
+
+    useEffect(() => {
+        randomDogImg()
+    }, [])
 
     return (
     <div className="hero-container">
@@ -9,7 +25,7 @@ const Landing = () => {
         <h1>Pawsitive Match</h1>
         <p>Find Your Furry Forever Friend!</p>
         <Link to={"/signup"}>
-            <button className="btn btn-outline-dark"><img src="" alt="randomDog" /></button>
+            <button className="btn btn-outline-dark"><img src={ dogSrc } alt="randomDog" /></button>
         </Link>
     </div>
     )
