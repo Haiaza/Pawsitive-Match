314aa5ce1ec33e1233ce52206008b9acea643915
haiaza <ajames1342@gmail.com>
2024-10-21 13:36:15 -0400

    Experimenting with layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { signOut } from '../services/authService';
 
 const Navbar = ({ user }) => {
 
     const userId = user ? user._id : null;
+
+    const handleSignOut = () => {
+
+            if (signOut()) {
+                console.log('User signed out successfully');
+            } else {
+                console.log('Sign-out failed');
+            }
+        }
 
     const location = useLocation()
 
@@ -15,11 +25,18 @@ const Navbar = ({ user }) => {
         <>
         { user ? (
                 
-            <nav>
-                <ul>
-                    <li><Link to="/dash">Dashboard</Link></li>
-                    <li><Link to="">Sign Out</Link></li>
-                    <li><Link to={`/profiles/${userId}`}>My Pets</Link></li> 
+            <nav className='navbar  bg-body-tertiary'>
+                <ul className='navbar-nav'>
+                    <div className="container-fluid">
+                        <Link className='navbar-brand' to="/dash">Pawsitive Match</Link>
+                        <li className='nav-item'>
+                            <Link className='nav-link'to='/signin' onClick={handleSignOut} >Sign Out</Link></li>
+                        <li className='nav-item'>
+                            <Link className='nav-link'to={`/profiles/${userId}`}>My Pets</Link></li>
+                        <li className='nav-item'>
+                            <Link className='nav-link' to="/pets/submit" >Add to the Sancutuary</Link>
+                        </li>
+                    </div>
                 </ul>
             </nav>
         ) :
