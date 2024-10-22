c569c0caec2abec06b4c5486cb22df45d0d6d8f9
haiaza <ajames1342@gmail.com>
2024-10-21 21:24:19 -0400

    ShowPet now has a way to be accessed aside from manual

--- src/pages/ShowPet.jsx
+++ src/pages/ShowPet.jsx
@@ -2,10 +2,13 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import petServices from '../services/petServices';
 import PetCard from '../components/PetCard';
+import { Link } from 'react-router-dom';
 
 const ShowPet = () => {
     const { id } = useParams(); // Get the pet ID from the URL
     const [pet, setPet] = useState(null);
+    
+    console.log(id)
 
     useEffect(() => {
         const fetchPet = async () => {
@@ -16,11 +19,14 @@ const ShowPet = () => {
         fetchPet();
     }, [id]);
 
+    const adoptionLink = window.location.href + '/adoption'
+    
     if (!pet) return <p>Loading...</p>;
 
     return (
         <>
             <PetCard pet={pet}/>
+            <button className='btn'><Link to={adoptionLink}><h3>Adopt me!</h3></Link></button>
         </>
     );
 };
